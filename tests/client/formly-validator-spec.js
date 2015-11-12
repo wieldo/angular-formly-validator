48e7148d7487cd1e11a192e96faa9a5dc9a9f2e3
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-12 18:02:28 +0100

    Add tests of setFieldValidator and getFieldValidator

--- tests/client/formly-validator-spec.js
+++ tests/client/formly-validator-spec.js
@@ -94,4 +94,106 @@ describe('formlyValidator', function () {
         }).not.toThrowError();
     });
 
+    it('should be able to set field validator', function () {
+        // empty field
+        var field = {
+            key: 'test'
+        };
+
+        formlyValidator.setFieldValidator(field, 'required', true);
+
+        expect(field).toEqual({
+            key: 'test',
+            transformers: {
+                validator: {
+                    required: true
+                }
+            }
+        });
+
+        // field with only transformers property
+
+        field = {
+            key: 'test',
+            transformers: {}
+        };
+
+        formlyValidator.setFieldValidator(field, 'required', true);
+
+        expect(field).toEqual({
+            key: 'test',
+            transformers: {
+                validators: {
+                    required: true
+                }
+            }
+        });
+
+        // field with one validator
+
+        field = {
+            key: 'test',
+            transformers: {
+                validators: {
+                    minlength: 3
+                }
+            }
+        };
+
+        formlyValidator.setFieldValidator(field, 'required', true);
+
+        expect(field).toEqual({
+            key: 'test',
+            transformers: {
+                validator: {
+                    required: true,
+                    minlength: 3
+                }
+            }
+        });
+
+    });
+
+    it('should be able to get field validator', function () {
+        var fields = [
+            {
+                field: {
+                    key: 'test'
+                },
+                expected: undefined
+            },
+            {
+                field: {
+                    key: 'test',
+                    transformers: {}
+                },
+                expected: undefined
+            },
+            {
+                field: {
+                    key: 'test',
+                    transformers: {
+                        validators: {}
+                    }
+                },
+                expected: undefined
+            },
+            {
+                field: {
+                    key: 'test',
+                    transformers: {
+                        validators: {
+                            required: true
+                        }
+                    }
+                },
+                expected: true
+            }
+        ];
+
+        _.each(fields, function (field) {
+            expect(formlyValidator.getFieldValidator(field.field, 'required')).toEqual(field.expected);
+        });
+    });
+
 });
